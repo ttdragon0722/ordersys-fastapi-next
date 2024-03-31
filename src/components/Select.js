ed3c14502fb38f7623c5a20d5f1880268ed1cf92
ttdragon0722 <a0909956502@gmail.com>
2024-03-31 11:49:00 +0800

    select debug

--- src/components/Select.js
+++ src/components/Select.js
@@ -5,14 +5,16 @@ import { useEffect, useState } from "react";
 
 const Select = () => {
     const { allCompany,companySelect,setCompanySelect } = useSourceContext();
+    const [companyChoose,setCompanyChoose] = useState(companySelect);
 
     const selectChange = (e) => {
         setCompanySelect(e.target.value);
+        setCompanyChoose(e.target.value);
     }
 
     return (
 
-        <select id="compony" value={companySelect} className="select px-5 py-1 rounded-lg font-bold shadow-lg bg-white/50 mx-2"
+        <select id="compony" value={companyChoose} className="select px-5 py-1 rounded-lg font-bold shadow-lg bg-white/50 mx-2"
             onChange={selectChange}>
             <option disabled value="DEFAULT">選擇廠商</option>
             {allCompany &&
